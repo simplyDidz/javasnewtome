3cdb3d07b87e877f5cf7b7ca58a7652d002f811e
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-04 13:58:48 +0100

    new changes

--- variables.js/loops.js
+++ variables.js/loops.js
@@ -1,13 +1,20 @@
 for(i =0 ; i <10 ; i++);
 
-Faveourite Choco = [
+Faveourite Choco = 
 
-"mars",
-"snickers",
-"kinderbueno",
+"mars";
+"snickers";
+"kinderbueno";
 "picnic"]
 
 for (choco index = 0; chocoIndex < favouriteChoco.legnth;chocoIndex++){
-console.log([favouritechoco[chocoIndex]);
+console.log(favouritechoco[chocoIndex]);
 }
 
+
+
+
+git add . 
+git commit -m 'your commit comment'
+git Push
+git status
